70305101709a3da5adf0f72e5cb7cf2cddd1d9c0
Brad Newman <newmanbradm@gmail.com>
2018-04-12 14:13:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+var number = 1
+
+function takeANumber(katzDeliLine) {
+  katzDeliLine.push(number++)
+  return number - 1
+}
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!"
@@ -18,6 +25,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
+  
   for (let i = 0; i < katzDeliLine.length; i++) {
     currentLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
   }
